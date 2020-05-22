269da98db53fa01ec2f89efc4e5d21f115c29cb8
rahulwatave <rahulwatave@gmail.com>
2020-05-22 19:53:35 -0400

    Added Welcome Test

--- src/main/java/com/bank/pages/WelcomePage.java
+++ src/main/java/com/bank/pages/WelcomePage.java
@@ -1,5 +1,32 @@
 package com.bank.pages;
 
-public class WelcomePage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.bank.base.Base;
+
+public class WelcomePage extends Base {
+	
+	@FindBy(id="signin_button")
+	WebElement signInBtn;
+	
+	public WelcomePage()
+	{
+		PageFactory.initElements(driver, this);
+	}
+
+	// what action to be perform: click on sign in button
+	
+
+	public LoginPage clksignIn() {
+		// TODO Auto-generated method stub
+		WebDriverWait wait=new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.elementToBeClickable(signInBtn));
+		signInBtn.click();
+		return new LoginPage();// this is not mandatory but for good connectivity to be used
+	}
 
 }
--- src/test/java/com/bank/testcases/WelcomeTest.java
+++ src/test/java/com/bank/testcases/WelcomeTest.java
@@ -1,5 +1,31 @@
 package com.bank.testcases;
 
-public class WelcomeTest {
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.bank.base.Base;
+import com.bank.pages.LoginPage;
+import com.bank.pages.WelcomePage;
+
+public class WelcomeTest extends Base {
+	WelcomePage wp;
+	LoginPage lp;
+	@BeforeMethod
+	public void setup() throws IOException
+	{
+		initialization();
+		wp=new WelcomePage();
+	}
+	@Test
+	public void verifySiginIn()
+	{
+		lp=wp.clksignIn();
+		String actual=driver.getTitle();
+		String extected="Zero - Log in";
+		Assert.assertEquals(actual, extected);
+	}
 
 }
